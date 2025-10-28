688c17ee5e2e45b0c9cb06ffdb35c3afb238fe74
Hugo <hugojamielniak@hotmail.com>
2025-10-28 18:43:55 -0300

    Add files via upload

---
+++ 02 - SEGUNDO PERÍODO/3 - PROGRAMAÇÃO WEB/TAREFAS/PHP/phpBasic.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Página</title>
+<style>
+	.botao{
+		padding: 10px, 15px;
+		margin: 5px;
+		border: 1px solid #333;
+		cursos: pointer;
+	}
+
+	.botao:hover{
+		background-color: blue;
+	}
+</style>
+</head>
+<body>
+
+
+
+
+
+
+
+	$nome="Teste";
+	echo "<h1>Seja bem vindo a página de $"
+</body>
+</html>
