c924fa7b05285089bc6d22047d21f9ce9def2023
Hugo <hugojamielniak@hotmail.com>
2026-04-23 09:43:38 -0300

    Add files via upload

--- 03 - PERÍODO/Performance em Sistemas Ciberfísicos/mapeamento/ProjetoMemoriaVirtual/ProjetoMemoriaVirtual/MapeamentoAssociativo.py
+++ 03 - PERÍODO/Performance em Sistemas Ciberfísicos/mapeamento/ProjetoMemoriaVirtual/ProjetoMemoriaVirtual/MapeamentoAssociativo.py
@@ -11,14 +11,45 @@ from Memoria import testaMapeamento
 # Retorno
 #    endereco que a pagina requisitada se encontra na memoriaPrincipal
 # Altere a funcao para fazer uso da tecnica de mapeamento associativo
+
+
+tabelaMapeamento =[ -1 for _ in range(0, 7)]
+
 def mapeamentoAssociativo(memoriaPrincipal: MemoriaPrincipal, memoriaSecundaria: MemoriaSecundaria, endereco: int) -> int:
     #quantidade de paginas em cada memoria =)
     qtPaginasMemoriaPrincipal = memoriaPrincipal.qtPaginas
     qtPaginasMemoriaSecundaria = memoriaSecundaria.qtPaginas
 
-    
-    #retorna endereco
+    global tabelaMapeamento
+    # TIRA ISSO AQUI -> global indice
+
+    # o índice não pode ser global, pq dai ao inves dele ser aleatorio
+    # vc vai aleatorizar ele uma única vez e usar o mesmo numero sempre
+    indice = random.randint(0, 7)
+
+    paginaRequisitada = endereco >> 2
+    BtyeRequisitado = endereco & 3
+
+
+    print("Pagina requisitada:", paginaRequisitada)
+    print("Byth requisitado:", BtyeRequisitado)
+    print("tabela de mapeamento", tabelaMapeamento)
+    print("valor", indice)
+
+    if not paginaRequisitada in tabelaMapeamento:
+        pagina = memoriaSecundaria.getPagina(paginaRequisitada)
+        memoriaPrincipal.setPagina(pagina, indice)
+        tabelaMapeamento[indice] = paginaRequisitada
+        return indice
+    else:
+        for i in range(0, len(tabelaMapeamento)):
+            if paginaRequisitada == tabelaMapeamento[i]:
+                return i
+
     return 0
+
+
+
 
 #Utilize esta funcao caso precise inicializar alguma variavel para o mapeamento =)
 def inicializaMapeamento(memoriaPrincipal: MemoriaPrincipal, memoriaSecundaria: MemoriaSecundaria):
