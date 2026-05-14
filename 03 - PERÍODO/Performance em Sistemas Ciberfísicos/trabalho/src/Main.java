9c4aabc601eefb290e6f8f40641911c4334a3bb2
Hugo <hugojamielniak@hotmail.com>
2026-05-14 08:17:58 -0300

    Add multithreading for filling and counting numbers

--- 03 - PERÍODO/Performance em Sistemas Ciberfísicos/trabalho/src/Main.java
+++ 03 - PERÍODO/Performance em Sistemas Ciberfísicos/trabalho/src/Main.java
@@ -1,3 +1,49 @@
+import java.util.Random;
+
+class ThreadPreencher extends Thread {
+    private double[] numeros;
+    private int inicio, fim;
+
+    public ThreadPreencher(double[] numeros, int inicio, int fim) {
+        this.numeros = numeros;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for (int i = inicio; i < fim; i++) {
+            numeros[i] = random.nextDouble();
+        }
+    }
+}
+
+class ThreadContar extends Thread {
+    private double[] numeros;
+    private int inicio, fim;
+    private int contadorParcial = 0;
+
+    public ThreadContar(double[] numeros, int inicio, int fim) {
+        this.numeros = numeros;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public void run() {
+        for (int i = inicio; i < fim; i++) {
+            if (numeros[i] > 0.25 && numeros[i] < 0.75) {
+                contadorParcial++;
+            }
+        }
+    }
+
+    public int getContadorParcial() {
+        return contadorParcial;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
         double[] numeros = new double[2_000_000];
 
 
@@ -35,3 +81,4 @@
         int totalCriterio = c1.getContadorParcial() + c2.getContadorParcial() + c3.getContadorParcial() + c4.getContadorParcial();
 
         System.out.println("Total de números entre 0.25 e 0.75: " + totalCriterio);
+    }
